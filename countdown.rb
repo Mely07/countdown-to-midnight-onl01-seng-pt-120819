638a8a39f21a28ce8ab487a97663219cb3b8c276
Damely Tineo <damely.tineo07@gmail.com>
2019-12-14 04:53:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown (number)
   while number > 0
   number -= 1
-  puts "#{integer} SECOND(S)!"
+  puts "#{number} SECOND(S)!"
   end
 puts "HAPPY NEW YEAR!"  
 end
@@ -11,7 +11,7 @@ end
 def countdown (number)
   while number > 0
   number -= 1
-  puts "#{integer} SECOND(S)!"
+  puts "#{number} SECOND(S)!"
   end
 puts "HAPPY NEW YEAR!"  
 end
